a5270aebaff8f80cba3e77b5e86dd39e541c401c
Dennis Acker <57709755+Lucksmith369@users.noreply.github.com>
2024-11-05 10:27:55 +0100

    Initial commit

---
+++ LsSkeleton/src/LsSkeleton.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace LsSkeleton;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+
+class LsSkeleton extends Plugin
+{
+    public function install(InstallContext $installContext): void
+    {
+        parent::install($installContext);
+    }
+
+}
