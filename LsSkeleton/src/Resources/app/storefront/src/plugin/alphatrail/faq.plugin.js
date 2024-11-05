2421864dec759eecf89cb6dea00847914515d84f
Wulf Schmoll <w.s@lucksmith.de>
2024-11-05 18:26:40 +0100

    wip

--- LsSkeleton/src/Resources/app/storefront/src/plugin/alphatrail/faq.plugin.js
+++ LsSkeleton/src/Resources/app/storefront/src/plugin/alphatrail/faq.plugin.js
@@ -1,8 +1,14 @@
 import Plugin from 'src/plugin-system/plugin.class'
+import DomAccess from 'src/helper/dom-access.helper'
+import Iterator from 'src/helper/iterator.helper'
 
 export default class FaqPlugin extends Plugin {
   static options = {
-    items: []
+    items: [],
+    collapseShowClass: 'show',
+    collapseColumnSelector: '.accordion',
+    collapseColumnTriggerSelector: '.faq-trigger',
+    collapseColumnContentSelector: '.faq-content',
   }
 
   init() {
@@ -11,10 +17,15 @@ export default class FaqPlugin extends Plugin {
     this.categorySelectEl = this.el.querySelector('#categories')
     this.listEl = this.el.querySelector('.items')
 
+    this._columns = this.el.querySelectorAll(this.options.collapseColumnSelector);
+
     this.initListeners()
   }
 
   initListeners() {
+    this._onViewportHasChanged()
+    document.addEventListener('Viewport/hasChanged', this._onViewportHasChanged.bind(this))
+
     this.searchBtnEl.addEventListener('click', this.onSearchBtnClick.bind(this))
   }
 
@@ -26,5 +37,51 @@ export default class FaqPlugin extends Plugin {
 
   search(query) {
     console.log(query)
+  }
+
+  _onViewportHasChanged() {
+    const event = 'click'
+
+    Iterator.iterate(this._columns, column => {
+        const trigger = DomAccess.querySelector(column, this.options.collapseColumnTriggerSelector)
+
+        // remove possibly existing event listeners
+        trigger.removeEventListener(event, this._onClickCollapseTrigger)
+
+        // add event listener
+        trigger.addEventListener(event, this._onClickCollapseTrigger.bind(this))
+    });
+
+    this.$emitter.publish('onViewportHasChanged')
+  }
+
+  /**
+     * On clicking the collapse trigger (column headline) the columns
+     * content area shall be toggled open/close
+     * @param {Event} event
+     * @private
+     */
+  _onClickCollapseTrigger(event) {
+    const trigger = event.target;
+    const collapseEl = trigger.parentNode.querySelector(this.options.collapseColumnContentSelector);
+    const collapseShowClass = this.options.collapseShowClass;
+
+    new bootstrap.Collapse(collapseEl, {
+        toggle: true,
+    });
+
+    collapseEl.addEventListener('shown.bs.collapse', () => {
+        trigger.classList.add(collapseShowClass);
+
+        this.$emitter.publish('onCollapseShown');
+    });
+
+    collapseEl.addEventListener('hidden.bs.collapse', () => {
+        trigger.classList.remove(collapseShowClass);
+
+        this.$emitter.publish('onCollapseHidden');
+    });
+
+    this.$emitter.publish('onClickCollapseTrigger');
   }
 }
