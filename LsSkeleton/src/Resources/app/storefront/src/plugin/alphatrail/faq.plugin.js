b876587d3e81a98181b9e1edfed774d9b413f0e3
Wulf Schmoll <w.s@lucksmith.de>
2024-11-05 22:15:59 +0100

    more accordion fixes, some styling

--- LsSkeleton/src/Resources/app/storefront/src/plugin/alphatrail/faq.plugin.js
+++ LsSkeleton/src/Resources/app/storefront/src/plugin/alphatrail/faq.plugin.js
@@ -17,38 +17,40 @@ export default class FaqPlugin extends Plugin {
     this.searchBtnEl = this.el.querySelector('#search-btn')
     this.categorySelectEl = this.el.querySelector('#categories')
     this.listEl = this.el.querySelector('.items')
-    this.cancelBtnEl = this.el.querySelector('#cancel')
-
+    // this.cancelBtnEl = this.el.querySelector('#cancel')
     this._columns = this.el.querySelectorAll(this.options.collapseColumnSelector)
-
     this.initListeners()
   }
 
   initListeners() {
     this._onViewportHasChanged()
     document.addEventListener('Viewport/hasChanged', this._onViewportHasChanged.bind(this))
-
     this.searchBtnEl.addEventListener('click', this.onSearchBtnClick.bind(this))
-    this.cancelBtnEl.addEventListener('click', this.cancel.bind(this))
+    // this.cancelBtnEl.addEventListener('click', this.cancel.bind(this))
     this.categorySelectEl.addEventListener('change', this.onCategorySelected.bind(this))
+    this.searchInputEl.addEventListener('keypress', this.onEnter.bind(this));
   }
 
   onSearchBtnClick() {
-    console.log('start search')
     this.search(this.searchInputEl.value)
+  }
+
+  onEnter(e) {
+    if(e.key === 'Enter') this.onSearchBtnClick()
   }
   
   search(query) {
     if(query) {
-      this.options.items = structuredClone(this.options.items.filter(e => e.question.toLowerCase().includes(query.toLowerCase()) || e.answer.toLowerCase().includes(query.toLowerCase())))
+      if(this.categorySelectEl.value) {
+        this.options.items = structuredClone(this.initialItems.filter(e => e.question.toLowerCase().includes(query.toLowerCase()) && e.category === this.categorySelectEl.value || e.answer.toLowerCase().includes(query.toLowerCase()) && e.category === this.categorySelectEl.value))
+      } else {
+        this.options.items = structuredClone(this.initialItems.filter(e => e.question.toLowerCase().includes(query.toLowerCase()) || e.answer.toLowerCase().includes(query.toLowerCase())))
+      }
     } else {
       this.options.items = structuredClone(this.initialItems)
     }
   
-    this.updateDom()
-    
-    console.log(this.options)
-    this._onViewportHasChanged()
+    this.updateDom()    
   }
 
   updateDom() {
@@ -65,7 +67,22 @@ export default class FaqPlugin extends Plugin {
           aria-expanded="true"
           role="listitem"
         >
-          ${item.question} <span>${item.category}</span>
+          <div class="title">
+            <span>${item.category}</span>
+            <div>${item.question}</div>
+          </div>
+          <div class="product-row-toggle">
+            <span class="toggle-plus-icon">
+              <span class="icon icon-plus">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24"><defs><path d="M11 11V3c0-.5523.4477-1 1-1s1 .4477 1 1v8h8c.5523 0 1 .4477 1 1s-.4477 1-1 1h-8v8c0 .5523-.4477 1-1 1s-1-.4477-1-1v-8H3c-.5523 0-1-.4477-1-1s.4477-1 1-1h8z" id="icons-default-plus"></path></defs><use xlink:href="#icons-default-plus" fill="#758CA3" fill-rule="evenodd"></use></svg>
+              </span>
+            </span>
+            <span class="toggle-minus-icon">
+              <span class="icon icon-minus">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24"><defs><path id="icons-default-minus" d="M3 13h18c.5523 0 1-.4477 1-1s-.4477-1-1-1H3c-.5523 0-1 .4477-1 1s.4477 1 1 1z"></path></defs><use xlink:href="#icons-default-minus" fill="#758CA3" fill-rule="evenodd"></use></svg>
+              </span>
+            </span>
+          </div>
         </div>
 
         <div id="collapse-${i}"
@@ -81,11 +98,20 @@ export default class FaqPlugin extends Plugin {
 
       this.listEl.append(el)
     })
+    
+    this._onViewportHasChanged()
   }
 
   onCategorySelected(evt) {
-    console.log('cat selected', evt.target.value)
-    this.options.items = structuredClone(this.initialItems.filter(e => e.category.toLowerCase() === evt.target.value.toLowerCase()))
+    if(evt.target.value) {
+      if(this.searchInputEl.value) {
+        this.options.items = structuredClone(this.initialItems.filter(e => e.question.toLowerCase().includes(this.searchInputEl.value.toLowerCase()) && e.category.toLowerCase() === evt.target.value.toLowerCase() || e.question.toLowerCase().includes(this.searchInputEl.value.toLowerCase()) && e.category.toLowerCase() === evt.target.value.toLowerCase()))
+      } else {
+        this.options.items = structuredClone(this.initialItems.filter(e => e.category.toLowerCase() === evt.target.value.toLowerCase()))
+      }
+    } else {
+      this.options.items = this.initialItems
+    }
     this.updateDom()
   }
 
@@ -94,13 +120,9 @@ export default class FaqPlugin extends Plugin {
     const event = 'click'
 
     Iterator.iterate(this._columns, column => {
-        const trigger = DomAccess.querySelector(column, this.options.collapseColumnTriggerSelector)
-
-        // remove possibly existing event listeners
-        trigger.removeEventListener(event, this._onClickCollapseTrigger)
-
-        // add event listener
-        trigger.addEventListener(event, this._onClickCollapseTrigger.bind(this))
+      const trigger = DomAccess.querySelector(column, this.options.collapseColumnTriggerSelector)
+      trigger.removeEventListener(event, this._onClickCollapseTrigger)
+      trigger.addEventListener(event, this._onClickCollapseTrigger.bind(this))
     });
 
     this.$emitter.publish('onViewportHasChanged')
@@ -136,9 +158,10 @@ export default class FaqPlugin extends Plugin {
     this.$emitter.publish('onClickCollapseTrigger');
   }
 
-  cancel() {
-    this.categorySelectEl.value = ''
-    this.options.items = structuredClone(this.initialItems)
-    this.updateDom()
-  }
+  // cancel() {
+  //   this.searchInputEl.value = ''
+  //   this.categorySelectEl.value = ''
+  //   this.options.items = structuredClone(this.initialItems)
+  //   this.updateDom()
+  // }
 }
